40654c32e168cb566d6befaf52b2117adb3acb50
Adriane maciel <adrianemaciel.developer@gmail.com>
2023-01-11 20:59:30 -0300

    filter color, size and quantity of shirts

--- main.js
+++ main.js
@@ -17,9 +17,40 @@ const categoryChild = people.filter((people) => people.age <= 12);
 console.log("categoryChild", categoryChild);
 
 const categoryAdolescent = people.filter(
-  (people) => people.age >= 12 && people.age <= 18
+  (people) => people.age >= 12 && people.age < 18
 );
 console.log("categoryAdolescent", categoryAdolescent);
 
 const categoryAdult = people.filter((people) => people.age >= 18);
 console.log("categoryAdult", categoryAdult);
+
+// -------------------------------------------------------------
+
+const shirts = [
+  { theAmount: 1, size: "P", color: "black" },
+  { theAmount: 2, size: "P", color: "black" },
+  { theAmount: 3, size: "P", color: "yellow" },
+  { theAmount: 4, size: "P", color: "yellow" },
+  { theAmount: 5, size: "P", color: "yellow" },
+  { theAmount: 6, size: "M", color: "black" },
+  { theAmount: 7, size: "M", color: "yellow" },
+  { theAmount: 8, size: "M", color: "yellow" },
+  { theAmount: 9, size: "G", color: "black" },
+  { theAmount: 10, size: "G", color: "yellow" },
+];
+console.log("shirts", shirts);
+
+const yellowShirts = shirts.filter((shirts) => shirts.color == "yellow");
+console.log("yellowShirts", yellowShirts);
+
+const blackShirts = shirts.filter((shirts) => shirts.color == "black");
+console.log("blackShirts", blackShirts);
+
+const quantityShirtsP = shirts.filter((shirts) => shirts.size == "P");
+console.log("quantityShirtsP", quantityShirtsP);
+
+const quantityShirtsM = shirts.filter((shirts) => shirts.size == "M");
+console.log("quantityShirtsM", quantityShirtsM);
+
+const quantityShirtsG = shirts.filter((shirts) => shirts.size == "G");
+console.log("quantityShirtsG", quantityShirtsG);
